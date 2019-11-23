0fff26141f0bfdc75931d6cc9dffb79dd070d6a7
Pedro R. Jiménez <countzyx@gmail.com>
2019-11-23 00:07:07 -0500

    Implemented Worker Process

---
+++ worker/main.mjs
@@ -0,0 +1,19 @@
+import redis from "redis";
+import keys from "./keys.mjs";
+
+const redisClient = redis.createClient({
+  hosts: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message, 10)));
+});
+sub.subscribe("insert");
